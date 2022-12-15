559c7c841d67b50c231b4773fd8c48a20fe97e05
mmotassi <mmotassi@L-FR-5CG1331CFL.corp.capgemini.com>
2022-12-15 12:09:28 +0100

    added method to string for age and name

--- src/org/mehdi/model/User.java
+++ src/org/mehdi/model/User.java
@@ -2,30 +2,27 @@ package org.mehdi.model;
 
 public class User {
 
-private  String name;
-private int age;
+    private String name;
+    private int age;
 
-public String getName() {
-    return name;
-}
+    public String getName() {
+        return name;
+    }
 
-public void setName(String name) {
-    this.name = name;
-}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-@Override
-public String toString() {
-    return "User [name=" + name + "]";
-}
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", age=" + age + "]";
+    }
 
+    public int getAge() {
+        return age;
+    }
 
-
-
-public int getAge() {
-    return age;
-}
-
-public void setAge(int age) {
-    this.age = age;
-}
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
